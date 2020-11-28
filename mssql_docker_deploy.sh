0a0e383b22a5ffc15a5100b55d2dc454ed1c561d
NiHaiden <40402114+NiHaiden@users.noreply.github.com>
2020-11-29 00:01:52 +0100

    Add EULA Accept Question

--- mssql_docker_deploy.sh
+++ mssql_docker_deploy.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-echo "SA Password: "
+echo "Do you accept the EULA of MSSQL-Server 2019?"
+echo "Answer with Y or N"
 
-read -s sa_password
+read eula_answer
+
+if [ "$eula_answer" == "Y" ]
+then
+
+ echo "SA Password: "
+
+ read -s sa_password
 
 
 
-sudo docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$sa_password" \
-   -p 1433:1433 --name sql1 -h sql1 \
-   -d mcr.microsoft.com/mssql/server:2019-latest
+ sudo docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$sa_password" \
+    -p 1433:1433 --name sql1 -h sql1 \
+    -d mcr.microsoft.com/mssql/server:2019-latest
+ echo "Container was successfully deployed. :)"
+ echo "Script created by Narvimpere"
+else
+  echo "You declined the license. Answer: $eula_answer"
+fi
 
-echo "Container successfully deployed"
-echo "Copyright Niklas Haiden (narv.icu) 2020"
+
