5bc9a9c8b8270899463cd1c2fd7ed04cea0ef5ad
Niklas Haiden <n.haiden@htlstp.at>
2021-03-30 17:00:25 +0200

    Rework debian script, include messages, include step to remove any remainder of docker, add docker compse install

--- docker-install-debian.sh
+++ docker-install-debian.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
+
+echo "Removing any existing remainder of Docker Components"
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+echo "Installing needed dependencies"
+
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common -y
+    software-properties-common \
+    curl -y
 
 
+echo "Downloading Docker GPG Key"
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+
+echo "Adding Docker Debian Repository"
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
+
+echo "Updating repository data and installing Docker"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+echo "Installing Docker-Compose and making it executable"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
