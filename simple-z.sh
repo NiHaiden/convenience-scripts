c4746a3fb22f0d0075d53c3e4a167425545abb25
Niklas Haiden <n.haiden@htlstp.at>
2021-03-30 17:48:53 +0200

    basic structure and commands in place

--- simple-z.sh
+++ simple-z.sh
@@ -2,7 +2,8 @@ echo "simple-z.sh, v0.1"
 echo "Automating the installation of the Z-Shell and the ohmyzsh Extension :^)"
 echo "Copyright 2021"
 
-
+user_found=0
+user_name="something"
 echo "Determining Linux Distribution"
 DISTRO = grep '^NAME' /etc/os-release
 
@@ -11,21 +12,31 @@ then
     
     echo "Installing needed dependencies"
 
-    sudo apt install curl -y
+    apt install curl -y
 
     echo "Installing zsh"
     apt install zsh -y 
 
 
+    while [ $user_found != 1 ]
+    do
+        echo "What is the name of the user for whom you want install oh-my-zsh and it's default template file?"
+        read user_name
+
+        if id "$user_name" &>/dev/null; then
+            user_found=1
+        else
+            user_found=0
+        fi
+    done
+
+
     echo "Changing default shell to zsh"
-    chsh -s $(which zsh)
+    su -c chsh -s $(which zsh) $user_name
 
-    echo "What is the name of the user for whom you want install oh-my-zsh and it's default template file?"
-    read user_name
-
-    echo "Installing oh-my-zsh"
-    su $user_name
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    echo "Installing oh-my-zsh"    
+    su -c sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended $user_name
+fi
 
 
 
