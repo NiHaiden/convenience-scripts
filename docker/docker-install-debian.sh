981c1dbb3ff2614ee35631daa6493de27780eae6
Niklas Haiden <n.haiden@htlstp.at>
2022-02-10 03:14:03 +0100

    Make Debian Script arch agnostic, update gpg adding process, update compose version

--- docker/docker-install-debian.sh
+++ docker/docker-install-debian.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "Removing any existing remainder of Docker Components"
+echo "If Docker Components are installed, they will be removed now."
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
-echo "Installing needed dependencies"
+echo "Installing needed dependencies."
+
+sudo apt-get update
 
 sudo apt-get install \
     apt-transport-https \
@@ -14,22 +16,21 @@ sudo apt-get install \
     curl -y
 
 
-echo "Downloading Docker GPG Key"
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+echo "Adding the Docker GPG Key to verify the Docker's signature."
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 
 echo "Adding Docker Debian Repository"
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "Updating repository data and installing Docker"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 echo "Installing Docker-Compose and making it executable"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 
