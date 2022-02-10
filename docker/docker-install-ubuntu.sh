8a2991314cd4096c627fe581ff556f0fabc6063b
Niklas Haiden <n.haiden@htlstp.at>
2022-02-10 03:16:20 +0100

    Arch agnostic, GPG Key process changed, new compose

--- docker/docker-install-ubuntu.sh
+++ docker/docker-install-ubuntu.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
 echo "Removing any existing remainder of Docker Components"
-sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
 
 echo "Installing needed dependencies"
 sudo apt-get install \
-    apt-transport-https \
     ca-certificates \
     curl \
-    gnupg-agent \
-    software-properties-common \
-    curl -y
+    gnupg \
+    lsb-release -y 
 
 echo "Downloading Docker GPG Key"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo "Adding Docker Ubuntu Repository"
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "Updating repository data and installing Docker"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 echo "Installing Docker-Compose and making it executable"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
